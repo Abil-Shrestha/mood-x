be6d8dbe44fa6a6e2194c83b8a8cfc57a69754ab
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2024-02-01 01:17:16 -0500

    API Routes Fix

--- Frontend/src/app/api/artists/route.ts
+++ Frontend/src/app/api/artists/route.ts
@@ -1,16 +1,16 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req: NextRequest, res: NextResponse) => {
-  const API_URL =
-    process.env.NODE_ENV === "production"
-      ? "https://mood-x-production.up.railway.app/"
-      : "http://localhost:5000/"
+// export const GET = async (req: NextRequest, res: NextResponse) => {
+//   const API_URL =
+//     process.env.NODE_ENV === "production"
+//       ? "https://mood-x-production.up.railway.app/"
+//       : "http://localhost:5000/"
 
-  try {
-    const response = await fetch(`${API_URL}/artists`)
-    const data = await response.json()
-    return data
-  } catch (error) {
-    console.error("Error:", error)
-  }
-}
+//   try {
+//     const response = await fetch(`${API_URL}/artists`)
+//     const data = await response.json()
+//     return data
+//   } catch (error) {
+//     console.error("Error:", error)
+//   }
+// }
--- Frontend/src/app/api/playlists/route.ts
+++ Frontend/src/app/api/playlists/route.ts
@@ -1,20 +1,20 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+// import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const API_URL =
-    process.env.NODE_ENV === "production"
-      ? "https://mood-x-production.up.railway.app/"
-      : "http://localhost:5000/"
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const API_URL =
+//     process.env.NODE_ENV === "production"
+//       ? "https://mood-x-production.up.railway.app/"
+//       : "http://localhost:5000/"
 
-      try {
-        const response = await fetch(`${API_URL}/playlists`);
-        const data = await response.json();
-        return res.status(200).json(data);
-      } catch (error) {
-        console.error('Error:', error);
-        return res.status(500).json({ error: 'Internal Server Error' });
-      }
-}
+//       try {
+//         const response = await fetch(`${API_URL}/playlists`);
+//         const data = await response.json();
+//         return res.status(200).json(data);
+//       } catch (error) {
+//         console.error('Error:', error);
+//         return res.status(500).json({ error: 'Internal Server Error' });
+//       }
+// }
